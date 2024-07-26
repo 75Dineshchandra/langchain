eaa32aa06057cc75cb60a5b94c5a04955815f137
Dinesh Chandra Gaddam <84858706+75Dineshchandra@users.noreply.github.com>
2024-07-26 16:31:03 +0530

    news_research_tool.py

---
+++ news_research_tool.py
@@ -0,0 +1,78 @@
+import os
+import streamlit as st
+import pickle
+import time
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from transformers import pipeline
+
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env (especially huggingface api key)
+
+st.title("RockyBot: News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+process_url_clicked = st.sidebar.button("Process URLs")
+file_path = "faiss_store_huggingface.pkl"
+
+main_placeholder = st.empty()
+qa_pipeline = pipeline("question-answering", model="distilbert-base-uncased-distilled-squad")
+
+if process_url_clicked:
+    # load data
+    loader = UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    # create embeddings and save it to FAISS index
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vectorstore_huggingface = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    time.sleep(2)
+
+    # Save the FAISS index to a pickle file
+    with open(file_path, "wb") as f:
+        pickle.dump(vectorstore_huggingface, f)
+
+query = main_placeholder.text_input("Question: ")
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            retriever = vectorstore.as_retriever()
+
+            # Define a function to use the Hugging Face QA pipeline
+            def huggingface_qa(question, context):
+                result = qa_pipeline(question=question, context=context)
+                return result['answer']
+
+            chain = RetrievalQAWithSourcesChain.from_custom_qa(
+                retriever=retriever,
+                qa_function=huggingface_qa
+            )
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
+
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
